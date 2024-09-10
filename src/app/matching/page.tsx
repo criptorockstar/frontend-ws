3022aacbf891c04438dd4574444890897e9e27f3
daridev <darideveloper@gmail.com>
2024-09-10 04:00:05 -0600

    feat (matching): use system username

--- src/app/matching/page.tsx
+++ src/app/matching/page.tsx
@@ -19,17 +19,13 @@ function Dashboard() {
   const router = useRouter()
   const isRunning = useRef(false)
 
-  // Redux data
+  // Get username from redux
   const dispatch = useDispatch()
   const user = useSelector((state: RootState) => state.user)
-
-  // Component state
-  const [username, setUsername] = useState('')
-  const [usernameSaved, setUsernameSaved] = useState(false)
+  console.log({ user })
 
   // Connext to game room and set username
   useEffect(() => {
-
     // Show error with sweetalert
     function showError(message: string) {
       Swal.fire({
@@ -46,68 +42,64 @@ function Dashboard() {
       })
     }
 
-    // Save username in redux
-    if (username && usernameSaved) {
-      dispatch(setUserState({ username }))
-
-
-      // Run only once
-      if (isRunning.current) {
-        return
-      }
-
-      // Connect to the websocket
-      const WS_HOST = process.env.NEXT_PUBLIC_WS_HOST
-      console.log('Connecting to the server: ', WS_HOST)
-      const wsEndpoint = `${WS_HOST}/matchmaker/`
-      const socket = new WebSocket(wsEndpoint)
-      isRunning.current = true
-
-      // Open connection
-      socket.onopen = function (e) {
-        // Show message when connection is open
-        console.log('Connected to the server. Waiting for players...')
-      }
-
-      // Redirect to the match page when a room is created
-      socket.onmessage = function (e) {
-        const data = JSON.parse(e.data)
-        const roomName = data['room_name']
-
-        Swal.fire({
-          title: 'Jugadores encontrados',
-          text: 'Serás redirigido a la partida',
-          icon: 'success',
-          confirmButtonText: 'Ir a la partida',
-        }).then((result: any) => {
-          if (result.isConfirmed) {
-            // Redirect to the match page with router
-            const page = `/duelo/${roomName}/`
-            router.push(page)
-          }
-        })
-      }
-
-      // Catch errors
-      socket.onclose = function (e) {
-        console.log('Connection closed.')
-      }
-
-      socket.onerror = function (e) {
-        showError(
-          'No se pudo conectar al servidor. Intente de nuevo más tarde.'
-        )
-      }
-
-      socket.onclose = function (e) {
-        showError(
-          'Se ha perdido la conexión con el servidor. Intente de nuevo más tarde.'
-        )
-      }
+    // Redirect to login page if user is not set
+    if (!user.username) {
+      router.push('/iniciar-sesion')
+      return
     }
-  }, [username, usernameSaved])
 
-  useEffect(() => {}, [])
+    // Run only once
+    if (isRunning.current) {
+      return
+    }
+
+    // Connect to the websocket
+    const WS_HOST = process.env.NEXT_PUBLIC_WS_HOST
+    console.log('Connecting to the server: ', WS_HOST)
+    const wsEndpoint = `${WS_HOST}/matchmaker/`
+    const socket = new WebSocket(wsEndpoint)
+    isRunning.current = true
+
+    // Open connection
+    socket.onopen = function (e) {
+      // Show message when connection is open
+      console.log('Connected to the server. Waiting for players...')
+    }
+
+    // Redirect to the match page when a room is created
+    socket.onmessage = function (e) {
+      const data = JSON.parse(e.data)
+      const roomName = data['room_name']
+
+      Swal.fire({
+        title: 'Jugadores encontrados',
+        text: 'Serás redirigido a la partida',
+        icon: 'success',
+        confirmButtonText: 'Ir a la partida',
+      }).then((result: any) => {
+        if (result.isConfirmed) {
+          // Redirect to the match page with router
+          const page = `/duelo/${roomName}/`
+          router.push(page)
+        }
+      })
+    }
+
+    // Catch errors
+    socket.onclose = function (e) {
+      console.log('Connection closed.')
+    }
+
+    socket.onerror = function (e) {
+      showError('No se pudo conectar al servidor. Intente de nuevo más tarde.')
+    }
+
+    socket.onclose = function (e) {
+      showError(
+        'Se ha perdido la conexión con el servidor. Intente de nuevo más tarde.'
+      )
+    }
+  }, [])
 
   return (
     <React.Fragment>
@@ -118,68 +110,29 @@ function Dashboard() {
         <div className='flex flex-col items-center pb-2'>
           {/* Cuadrado 1 */}
           <div className='relative w-md h-auto pt-24 px-10 py-8 mb-[50px] backdrop-blur-xl bg-primary-200 rounded-[10px] border-secondary border-[3px] flex flex-col items-center justify-center text-center cursor-pointer hover:bg-primary-100'>
-            {username && usernameSaved ? (
-              // Loading spinner
-              <>
-                <Image
-                  src='/spinner.svg'
-                  alt=''
-                  width={100}
-                  height={100}
-                  className='mt-[-130px]'
-                />
-                <div className='mt-5 flex justify-center'>
-                  <p
-                    className={`text-white text-[17px] xl:text-[22px] ${fonts.bowlbyOneSC.className}`}
-                  >
-                    Buscando jugadores
-                  </p>
-                </div>
-                <div className='flex justify-center items-center'>
-                  <p
-                    className={`text-white text-left text-[12px] xl:text-[16px] ${fonts.almarai.className} pr-[4px] text-center`}
-                  >
-                    Espere un momento mientras buscamos jugadores para una
-                    partida...
-                  </p>
-                </div>
-              </>
-            ) : (
-              // Username input
-              <>
-                <Input
-                  type='text'
-                  placeholder='Nombre de usuario'
-                  onChange={(e) => setUsername(e.target.value)}
-                  value={username}
-                  className={`
-                    text-white
-                    placeholder:opacity-80
-                  `}
-                />
-                <Button
-                  variant='default'
-                  size='default'
-                  className={`
-                  text-black
-                    bg-yellow hover:bg-yellow
-                    my-4 
-                    px-6
-                    py-2
-                    text-lg 
-                    opacity-100 hover:opacity-80
-                    scale-100 hover:scale-95
-                    duration-150
-                    transition
-                    rounded-xl
-                    font-bold
-                  `}
-                  onClick={() => setUsernameSaved(true)}
-                >
-                  Buscar partida
-                </Button>
-              </>
-            )}
+            {/* Loading spinner */}
+            <Image
+              src='/spinner.svg'
+              alt=''
+              width={100}
+              height={100}
+              className='mt-[-130px]'
+            />
+            <div className='mt-5 flex justify-center'>
+              <p
+                className={`text-white text-[17px] xl:text-[22px] ${fonts.bowlbyOneSC.className}`}
+              >
+                Buscando jugadores
+              </p>
+            </div>
+            <div className='flex justify-center items-center'>
+              <p
+                className={`text-white text-left text-[12px] xl:text-[16px] ${fonts.almarai.className} pr-[4px] text-center`}
+              >
+                Espere un momento mientras buscamos jugadores para una
+                partida...
+              </p>
+            </div>
           </div>
         </div>
         <LobbyUI />
